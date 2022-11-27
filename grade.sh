be145b71ca5455f5b9087ef8ff2114cbb97b5cb7
Ken Qin <114203676+k6qin@users.noreply.github.com>
2022-11-27 15:34:58 -0800

    grader

--- grade.sh
+++ grade.sh
@@ -2,30 +2,52 @@
 
 # set -e // dont run errors
 
+echo "Grading Script"
+echo "URL: $1"
+
 rm -rf student-submission
 git clone $1 student-submission
+
+if [$? -eq 0] 
+then 
+    echo "Successfully Cloned"
+else 
+    echo "Clone failed"
+    exit 1
+fi
+
+cd student-submission
 
 if [-e "ListExamples.java"]
 then
     echo "Correct file found"
 else 
     echo "Wrong file submitted"
-    exit 0
+    exit 1
 fi
 
-cp TestListExample.java /student-submission
+CP = ".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+cp ../TestListExample.java /student-submission
+javac -cp $CP *.java 2 > compilation_error.txt
+Message = `cat compilation_error.txt | head -1`
 
-CP = .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
-javac -cp $CP *.java
-
-$command 2 > file.txt
 if [$? -eq 0]
 then 
     echo "compilation succeed"
 else 
-    echo "compilation fails"
-    exit 0
+    echo "compilation failed. ($Message)"
+    exit 1
 fi
 
-java -cp $CP org.junit.runner.JUnitCore TestListExamples
-$command > output.txt
+rm -rf test_result.txt
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > test_result.txt
+
+if [$? -eq 0]
+then 
+    result_pass = `cat test_result.txt | grep "OK" `
+    echo "[TESTS PASSED] JUnit result: " $result_pass
+else 
+    result_fail = `cat test_result.txt | grep "Tests run:" `
+    echo "[TESTS FAILED] JUnit result: " $result_fail
+    exit 1
+fi
